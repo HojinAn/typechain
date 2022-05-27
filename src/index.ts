334f4b82cf7df50b8e9c3f9232fd3fd9b6cc7854
HojinAn <asdv94@naver.com>
2022-05-27 22:09:25 +0900

    5.4 JSDoc

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,3 @@
-import { init, exit } from "myPackage";
-init({ url: "true" });
-exit(1)
+import { init, exit } from "./myPackage";
+
+init();
--- src/myPackage.js
+++ src/myPackage.js
@@ -1,6 +1,19 @@
+// @ts-check
+/**
+ * Initialize the project
+ * @param {object} config
+ * @param {boolean} config.dubug
+ * @param {string} config.url
+ * @returns boolean
+ */
 export function init(config) {
   return true;
 }
+/**
+ * Exit the program
+ * @param {number} code
+ * @returns number
+ */
 export function exit(code) {
   return code + 1;
 }
