2433131209a58fa09840d23fe2080d3a8a520010
HojinAn <asdv94@naver.com>
2022-05-28 23:49:58 +0900

    5.7 Chain

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import crypto from "crypto";
 interface BlockShape {
   hash: string;
   prevHash: string;
-  heigt: number;
+  height: number;
   data: string;
 }
 class Block implements BlockShape {
@@ -20,3 +20,35 @@ class Block implements BlockShape {
     return crypto.createHash("sha256").update(toHash).digest("hex");
   }
 }
+
+class BlockChain {
+  private blocks: Block[];
+  constructor() {
+    this.blocks = [];
+  }
+  private getPrevHash() {
+    if (this.blocks.length === 0) return "";
+    return this.blocks[this.blocks.length - 1].hash;
+  }
+  public addBlock(data: string) {
+    const newBlock = new Block(
+      this.getPrevHash(),
+      this.blocks.length + 1,
+      data
+    );
+    this.blocks.push(newBlock);
+  }
+  public getBlocks() {
+    return [...this.blocks];
+  }
+}
+
+const blockchain = new BlockChain();
+
+blockchain.addBlock("First one");
+blockchain.addBlock("Second one");
+blockchain.addBlock("Third one");
+
+blockchain.getBlocks().push(new Block("xxxxx", 111111, "HACKED"));
+
+console.log(blockchain.getBlocks());
